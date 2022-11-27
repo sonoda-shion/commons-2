952fb7fdf0a56631f151e44d2ec898a71cf3ac51
sonoda-shion <xcgikdue2z@i.softbank.jp>
2022-11-27 10:17:26 +0900

    ✨ complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,67 @@
+// HAMBURGER
+$(function () {
+    $("#js-btn").on("click", function () {
+      $(".js-menu-btn").toggleClass("active");
+      $("#js-nav").toggleClass("show");
+      $('body').toggleClass('open');
+    });
+    $(".cross").on("click",function() {
+        $("#js-nav").removeClass("show");
+        $('body').removeClass('open');
+    })
+});
 
+//*============= FOLLOWING =============*/
+jQuery(function($) {
+    var $win = $(window),
+        $nav = $('.header'),
+        navPos = $nav.offset().top,
+        fixedClass = 'fixed';
+    $win.on('load scroll', function() {
+      var value = $(this).scrollTop();
+      if ( value > navPos + 80 ) {
+        $nav.addClass(fixedClass);
+        $contactBtn.addClass(fixedClass);
+      } else {
+        $nav.removeClass(fixedClass);
+        $contactBtn.removeClass(fixedClass);
+      }
+    });
+});
+  
+
+// downMenu
+$(function() {
+    $(".click-1").on("click",function() {
+        $(".click-1").toggleClass("rotate")
+        $(".down-1").toggleClass("active");
+    });
+    $(".click-2").on("click",function() {
+        $(".click-2").toggleClass("rotate")
+        $(".down-2").toggleClass("active");
+    });
+    $(".click-3").on("click",function() {
+        $(".click-3").toggleClass("rotate")
+        $(".down-3").toggleClass("active");
+    });
+    $(".click-4").on("click",function() {
+        $(".click-4").toggleClass("rotate")
+        $(".down-4").toggleClass("active");
+    });
+    $(".click-5").on("click",function() {
+        $(".click-5").toggleClass("rotate")
+        $(".down-5").toggleClass("active");
+    });
+    $(".click-6").on("click",function() {
+        $(".click-6").toggleClass("rotate")
+        $(".down-6").toggleClass("active");
+    });
+});
+
+// SCROLLREVEAL
+ScrollReveal().reveal('.reveal', {
+    duration: 1000, 
+    origin: 'top', 
+    distance: '50px',  
+});
 
